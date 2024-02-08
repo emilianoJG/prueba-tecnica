c8d5350346592dc9f52f30c12e581fc13902037c
ruralSantaFe <ruralsantafe19@gmail.com>
2024-02-08 11:19:43 -0300

    0.1

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace PruebaTecnica
+{
+    class Program
+    {
+        /*
+        EJERCICO Nº 1:
+            Obtener la cantidad de caracteres que contiene "miString"
+            string miString = "Entrevista laboral de Rural Santa Fe";
+            
+        static void Main()
+        {
+          
+        }
+        */
+        
+
+        /*
+        EJERCICO Nº 2:
+            Explicar que realiza el siguiente codigo:
+
+        static void Main()
+        {
+            Console.Write("Ingrese un número: ");
+            string input = Console.ReadLine();               
+                if (int.TryParse(input, out int number))
+                {                    
+                    EvaluarNumero(number);
+                }
+                else
+                {
+                    Console.WriteLine("Por favor, ingrese un número válido.");
+                }
+        }                                    
+        static void EvaluarNumero(int numero)
+            {                
+                if (numero % 2 == 0)
+                {
+                    Console.WriteLine("El número ingresado es par.");
+                }
+                else
+                {
+                    Console.WriteLine("El número ingresado es impar.");
+            }
+        }
+        */
+
+        /*
+        EJERCICIO Nº 3:
+            Usando la siguiente conexión con la API, obtener mediante GET los productos almacenados.
+            string apiUrl = "http://ruralsantafe.no-ip.org:123/api/Productos";
+        
+        static void Main()
+        {
+          
+        }
+        */
+    }
+}
